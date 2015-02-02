2a64d8614172dca34846d9c8c0c31e66105d6a78
AlMos321 <alexmoss09@gmail.com>
2015-02-02 14:25:01 +0200

    fix some problem

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "My first maven project!" );
+        System.out.println( "My first maven project" );
     }
 }
