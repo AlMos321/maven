16f6b981b52c7659cb24fd6596b8b285aa58c651
AlMos321 <alexmoss09@gmail.com>
2015-02-04 12:25:43 +0200

    bag fix

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "My first maven project" );
+        System.out.println( "My first maven projects" );
+        System.out.println( "some action" );
+
     }
 }
---
+++ src/main/java/com/mycompany/app/My.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+/**
+ * Created by user on 02.02.15.
+ */
+public class My {
+    public static void main( String[] args )
+    {
+        System.out.println( "My class" );
+        System.out.println( "My class" );
+
+    }
+}
